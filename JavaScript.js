5b76ee51018e533eb69a5059be90c5838150a531
Jakob Lamber <mr.jamlamb@gmail.com>
2022-12-03 15:39:24 -0500

    Implement all functionality but zero

--- JavaScript.js
+++ JavaScript.js
@@ -42,9 +42,74 @@ function operate(num1,num2,operator){
 //in the next step.
 
 const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const text = document.createElement("p");
+const answer = document.createElement("p");
+screen.append(text);
+screen.append(answer);
+let secondHalf = false;
+let value1 = "";
+let value2 = "";
+let operator = "";
+
+
 btns.forEach(btn => btn.addEventListener("click", function(){
-    alert(btn.value)
+    //alert(btn.value);
+    if(isNaN(btn.value) && btn.value != "."){
+        //if it is not a number (meaning that it is an operator)
+        //we want to then check if there are numbers in text already
+        if(btn.value == "="){
+            //alert(operate(parseInt(value1), parseInt(value2), operator));
+            answer.textContent = (operate(Number(value1), Number(value2), operator)).toFixed(2);
+
+        }
+        else if(btn.value == "AC"){
+            value1 = "";
+            value2 = "";
+            operator = "";
+            text.textContent ="";
+            answer.textContent="";
+            secondHalf = false;
+
+        }
+        else if(isNaN(screen.firstChild.textContent) || screen.firstChild.textContent == ""){
+            //this catches an error if you try and type 2 operators back to back
+            //also catches if you begin with an operator
+            alert("ERROR");
+        }
+        else if(screen.firstChild){
+            //this checks if it atleast has 1 child
+            //if it does we want to store this value as value 1
+            value1 = screen.firstChild.textContent;
+            text.textContent += ` ${btn.value} `
+            operator = `${btn.value}`;
+            //alert(value1);
+            secondHalf=true;
+        }
+    }
+    else{
+        //maybe have a boolean to see if it is second half of eqn
+        if(secondHalf == true){
+            text.textContent += `${btn.value}`
+            value2 += btn.value;
+            //alert(value2);
+        }
+        else{
+            text.textContent += `${btn.value}`
+            //screen.append(text);
+        }
+    }
 }));
+/*
+const specialBtns = document.querySelectorAll(".specialBtn");
+specialBtns.forEach(btn => btn.addEventListener("click", function(){
+    if(btn.value == "="){
+        alert(operator, value1,value2)
+        //alert(operate(parseInt(value1), parseInt(value2), operator));
+    }
+}));
+*/
+
 
 
 //i guess i can implement all of this into a stack?

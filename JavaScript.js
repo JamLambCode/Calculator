fa0002f1e473f9a101251eb9408ff4b8d65d9787
Jakob Lamber <mr.jamlamb@gmail.com>
2022-12-04 15:00:07 -0500

    Base calculator done

--- JavaScript.js
+++ JavaScript.js
@@ -11,7 +11,7 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return (b==0) ? "ERROR" : a/b;
+    return (b==0) ? "you do know you can't do that... right?" : a/b;
 }
 
 function operate(num1,num2,operator){
@@ -54,83 +54,108 @@ let operator = "";
 
 
 btns.forEach(btn => btn.addEventListener("click", function(){
-    //alert(btn.value);
-    if(isNaN(btn.value) && btn.value != "."){
-        //if it is not a number (meaning that it is an operator)
-        //we want to then check if there are numbers in text already
-        if(btn.value == "=" || (isNaN(btn.value) &&  (value2 != "") && btn.value != "AC")){
-            //we just check if it operates a string first and foremost bc that means its an error
-            if(typeof(operate(Number(value1), Number(value2), operator)) != "string"){
-                (Number.isInteger((operate(Number(value1), Number(value2), operator)))) 
-                ? answer.textContent = (operate(Number(value1), Number(value2), operator))
-                : answer.textContent = (operate(Number(value1), Number(value2), operator)).toFixed(2);
-            }
-            else{
-                answer.textContent = (operate(Number(value1), Number(value2), operator));
-            }
-            if(btn.value != "="){ //this is to evaluate if you continue evaluating
-                value1 = answer.textContent;
-                answer.textContent="";
-                text.textContent =""
-                value2 = "";
-                operator = `${btn.value}`;
-                text.textContent += value1 +  " " + operator + " ";
-
-            }
+    //since this is checked first we want the answer to be assessed as blank
+    if(btn.value == "~" && answer.textContent == ""){
+        if(value2 != ""){ //this checks if value2 has been set or not to decide which value we must switch the sign for
+            //this means that we are changing the sign of value 2
+            (Number(value2) > 0 ) ? value2 = "-" + value2 : value2 = Math.abs(value2); 
+            
+            //we change value 2 and then update the screen
+            text.textContent = `${value1} ${operator} ${value2}`;
         }
-        else if(btn.value == "AC"){
-            value1 = "";
-            value2 = "";
-            operator = "";
-            text.textContent ="";
-            answer.textContent="";
-            secondHalf = false;
-
-        }
-        else if(isNaN(screen.firstChild.textContent) || screen.firstChild.textContent == ""){
-            //this catches an error if you try and type 2 operators back to back
-            //also catches if you begin with an operator
-            alert("ERROR,2");
-        }
-        else if(screen.firstChild){
-            //this checks if it atleast has 1 child
-            //if it does we want to store this value as value 1
-            value1 = screen.firstChild.textContent;
-            text.textContent += ` ${btn.value} `
-            operator = `${btn.value}`;
-            //alert(value1);
-            secondHalf=true;
+        else{
+            //we need to make sure that we still don't have an operator so we know we are working on the first number
+            if(operator == ""){
+                alert(value1);
+                (Number(value1) > 0 ) ? value1 = "-" + value1 : value1 = Math.abs(value1); 
+                text.textContent = `${value1}`;
+            }
         }
     }
     else{
-        //maybe have a boolean to see if it is second half of eqn
-        if(secondHalf == true){
-            text.textContent += `${btn.value}`
-            value2 += btn.value;
-            //alert(value2);
+        if(isNaN(btn.value) && btn.value != "."){
+            //if it is not a number (meaning that it is an operator)
+            //we want to then check if there are numbers in text already
+            if(btn.value == "=" || (isNaN(btn.value) &&  (value2 != "") && btn.value != "AC")){
+                
+                if(value2 == ""){
+                    //we want to do nothing if we don't have 2 values to operate on
+                }
+                else{
+                    //we just check if it operates a string first and foremost bc that means its an error
+                    if(typeof(operate(Number(value1), Number(value2), operator)) != "string"){
+                        (Number.isInteger((operate(Number(value1), Number(value2), operator)))) 
+                        ? answer.textContent = (operate(Number(value1), Number(value2), operator))
+                        : answer.textContent = (operate(Number(value1), Number(value2), operator)).toFixed(2);
+                    }
+                    else{
+                        answer.textContent = (operate(Number(value1), Number(value2), operator));
+                    }
+                    if(btn.value != "=" && btn.value != "~" && !(isNaN(answer.textContent))){ //this is to evaluate if you continue evaluating
+                        value1 = answer.textContent;
+                        answer.textContent="";
+                        text.textContent =""
+                        value2 = "";
+                        operator = `${btn.value}`;
+                        text.textContent += value1 +  " " + operator + " ";
+
+                    }
+                }
+            }
+            else if(btn.value == "AC"){
+                value1 = "";
+                value2 = "";
+                operator = "";
+                text.textContent ="";
+                answer.textContent="";
+                secondHalf = false;
+
+            }
+            else if(isNaN(screen.firstChild.textContent) || screen.firstChild.textContent == ""){
+                //this catches an error if you try and type 2 operators back to back
+                //also catches if you begin with an operator
+                alert("ERROR");
+            }
+            else if(screen.firstChild){
+                //this checks if it atleast has 1 child
+                //if it does we want to store this value as value 1
+                //value1 = screen.firstChild.textContent;
+                text.textContent += ` ${btn.value} `
+                operator = `${btn.value}`;
+                //alert(value1);
+                secondHalf=true;
+            }
         }
         else{
-            text.textContent += `${btn.value}`
-            //screen.append(text);
+            //maybe have a boolean to see if it is second half of eqn
+            if(secondHalf == true && answer.textContent == ""){
+                if(btn.value == "."){
+                    if(!(value2.includes("."))){ //we are ignoring if it already has a decimal
+                        value2 += btn.value
+                        text.textContent += `${btn.value}`;
+                    }
+                }
+                else{
+                    value2 += btn.value;
+                    text.textContent += `${btn.value}`
+                    
+                //alert(value2);
+                }
+            }
+            else{ //this is value1
+                if(answer.textContent == "") { //making sure that we don't allow value2 to change once we have the answer
+                    if(btn.value == "."){
+                        if(!(value1.includes("."))){ //we are ignoring if it already has a decimal
+                            value1 += btn.value
+                            text.textContent += `${btn.value}`;
+                        }
+                    }
+                    else{
+                        value1 += btn.value;
+                        text.textContent += `${btn.value}`
+                    }
+                }
+            }
         }
     }
 }));
-/*
-const specialBtns = document.querySelectorAll(".specialBtn");
-specialBtns.forEach(btn => btn.addEventListener("click", function(){
-    if(btn.value == "="){
-        alert(operator, value1,value2)
-        //alert(operate(parseInt(value1), parseInt(value2), operator));
-    }
-}));
-*/
-
-
-
-//i guess i can implement all of this into a stack?
-//and then through the stack I can check if there are enough
-//numbers to do the operation with and if there aren't then 
-//idk
-//i guess i can implement it with two stack
-//one stack with the numbers and the other stack with all of
-// the operators
